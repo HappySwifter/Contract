5a211d039e10622cf810f8050b4aeff7feba853d
Artem <artem.valiev@gmail.com>
2018-11-24 14:57:00 +0300

    Initial Commit

---
+++ SmartContrakt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartContrakt
+//
+//  Created by Артем Валиев on 24/11/2018.
+//  Copyright © 2018 Артем Валиев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
